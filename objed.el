60c47318a11e904ea79ab3bf399b70566c20d714
Clemera <clemera@posteo.net>
2018-11-25 14:54:27 +0100

    Remove key binding info in header, use readme for that

--- objed.el
+++ objed.el
@@ -91,126 +91,20 @@
 ;; `objed-define-object'.
 ;;
 ;; Although some features are still experimental the basic user interface will
-;; stay the same. The following gives an overview of available keys and
-;; commands:
-;;
-;; Basic movement commands (switch the object type on movement):
-;;
-;; [`objed-map']
-;; f/b: Move forward/backward one character and activate the char object.
-;; s/r: Move forward/backward one word and activate the word object (*).
-;; S/R: Move forward/backward one symbol and activate the symbol object.
-;;      Skips strings and comments.
-;; n/p: Move to the next/previous line and activate the line object.
-
-;; (*): Emacs uses the f/b keys for word AND character movement (depending on
-;;      the used modifier). I made the decision to remap M-f, M-b to M-s, M-r in
-;;      my personal configuration (using `key-translation-map'). This is by no
-;;      means necessary but might be something to think about. Afterwards you
-;;      are able to start word movement with M-s/r and continue moving by words
-;;      with s/r which is more convenient than switching between M-b/f and s/r.
-;;      You can still access M-s/r regular bindings by using Meta-shift (M-S/R).
-;;
-;; Commands for block objects (objects built out of lines of text):
-;;
-;; [`objed-map']
-;; l  : Activate (line based) object at point and move to its start.
-;;      On repeat proceed to beginning of the indentation block, comment block,
-;;      paragraph or other block objects.
-;; a  : Move to beginning of line and activate the text moved over.
-;;      On repeat proceed to beginning of blocks like explained above.
-;; e  : Move to end of line and activate the text moved over.
-;;      On repeat proceed to end of blocks like explained above.
-;;
-;; Commands for context objects. Those objects are common programming constructs
-;; like defuns, strings, parentheses but also sentences inside comments for
-;; example. Any whitespace after point is skipped before determine the context:
-;;
-;; [`objed-map']
-;; t/h: If coming from basic movement the object at point is guessed
-;;      based on context. Point is moved  to the beginning/end of object.
-;;      This is useful to "jump out" of the current context.
-;;      On repeat move to the previous/next instance of current object type
-;; o  : Activate the inner part of the object at point and move to the start.
-;;      This is useful to act on the content of the string, brackets and so on.
-;;      On repeat expand to other objects around current position.
-;; u  : Move to end of the inner object at point and activate the text moved over.
-;;      On repeat toggle between beginning/end inside the object.
-;; q  : Move point to the other side of the current object.
-;; i  : Toggle the state of the current object (inner/whole).
-;;
-;; Commands to switch to other objects (and move point to its start):
-;;
-;; [`objed-map']
-;; c  : Prefix to switch to other objects,
-;;      see `objed-object-map' for available objects
-;;      and `objed-define-object' to add your own.
-;;
-;; Some objects are directly accessible without a prefix:
-;;
-;; .  : Switch To identifier object.
-;; _  : Switch to symbol object.
-;; %  : Switch to content object (defined by textual content of current object).
-;;
-;; Commands to edit objects (applying operations to them). When the region is
-;; active the operation acts on the current region. To act on multiple objects
-;; at once you can mark them first (see the "Misc commands" below):
-;;
-;; [`objed-map']
-;; x  : Prefix to access other operations,
-;;      see `objed-op-map' for available operations
-;;      and `objed-define-op' to add your own.
-;;
-;; Some operations are accessible without a prefix:
-;;
-;; k  : Kill current object(s).
-;;      Merges marked objects if they are only separated by whitespace.
-;; d  : Delete current object(s).
-;;      Merges marked objects if they are only separated by whitespace.
-;; w  : Copy current object(s).
-;; y  : Yank last killed text at point.
-;; TAB: Indent object(s).
-;; ;  : Un/comment object(s).
-;; :  : Comment and duplicate object.
-;; $  : Flyspell region.
-;;
-;; `      : Prefix to surround object(s) with any pair using `electric' (built-in).
-;; "([{   : Surround object with corresponding chars using `electric',
-;;
-;; Misc commands:
-;;
-;; [`objed-map']
-;; ,    : Pop to last state, which restores the last position and any object data.
-;; j    : Choose an instance of current object type with completion,
-;;        using the contents of the first line for completion.
-;; z    : Choose an instance of current object type on the screen with `avy'.
-;; m    : Add/Remove current object to marked objects and move to next.
-;; M    : Add/Remove current object to marked objects and move to previous.
-;; v    : Scroll up.
-;; V/M-v: Scroll down.
-;; /    : Undo last edit command.
-;; C-SPC: Set mark.
-;; C-h  : Get key binding help (uses `which-key' if available).
-;; g/C-g: Exit and deactivate `objed'.
-;;
-;; Dispatch keys (dispatch to any object defined in `objed-object-map'):
-;;
-;; [You can add your own prefix bindings using `objed-define-dispatch']
-;;  *  : Mark all instances of current object inside another object type.
-;;  <  : Activate part from point backward until boundary of some object.
-;;  >  : Activate part from point forward until boundary of some object.
+;; stay the same. 
 ;;
 ;;
 ;; CONTRIBUTE:
 ;;
-;; I'm happy to receive pull requests or ideas to improve this package. Some
-;; parts suffer from the bottom up approach of developing it, but this also
-;; allowed me to experiment a lot and try ideas while working on them, something
-;; that Emacs is especially good at. Most of the features are tested using
-;; `emacs-lisp-mode' but hopefully there aren't to many problems using modes for
-;; other languages, I tried my best to write text objects in a language agnostic
-;; way by using Emacs syntax information. Testing this and writing tests in
-;; general would be an important next step.
+;; I'm happy to receive pull requests or ideas to improve this
+;; package. Some parts suffer from the bottom up approach of
+;; developing it, but this also allowed me to experiment a lot and try
+;; ideas while working on them, something that Emacs is especially
+;; good at. Most of the features are tested using `emacs-lisp-mode'
+;; but hopefully there aren't to many problems using modes for other
+;; languages, I tried my best to write text objects in a language
+;; agnostic way. Testing this and writing more tests in general would
+;; be an important next step.
 ;;
 ;; This package would never been possible without the helpful community around
 ;; Emacs. Thank you all and see you in parendise...Share the software!
