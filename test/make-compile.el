10de1adcf60b2677c13390e1ba3b4e15b9e8a7cb
Clemera <clemera@posteo.net>
2018-12-02 10:55:59 +0100

    Comment about commandline test

--- test/make-compile.el
+++ test/make-compile.el
@@ -1,6 +1,9 @@
+;; emacs -Q -batch -L . -f batch-byte-compile file.el
+
 ;;  bail out on compilation warnings and errors
 (setq byte-compile-error-on-warn t)
 (setq byte-compile--use-old-handlers nil)
+
 
 ;; compile *.el files
 (dolist (file (file-expand-wildcards "*.el"))
