98a3a3536a10cebb617852d9db3a94217c3e7c27
Clemens Radermacher <clemera@posteo.net>
2019-03-04 13:45:19 +0100

    Vesion bump

--- objed.el
+++ objed.el
@@ -3,7 +3,7 @@
 
 ;; Author: Clemens Radermacher <clemera@posteo.net>
 ;; Package-Requires: ((emacs "25") (cl-lib "0.5"))
-;; Version: 0.6
+;; Version: 0.7
 ;; Keywords: convenience
 ;; URL: https://github.com/clemera/objed
 
