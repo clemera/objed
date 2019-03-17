7968f683fe06ed0a38bd661da7d9bb957e609b8d
Clemens Radermacher <clemera@posteo.net>
2019-03-17 20:01:35 +0100

    Formatting fixes

--- objed.el
+++ objed.el
@@ -50,34 +50,36 @@
 ;; object, change the object state or switch to other object types.
 ;;
 ;; The object state is either "inner" or "whole" and is indicated in the
-;; modeline by (i) or (w) after the object type. With inner state, anything that
-;; would be considered delimiters or padding around an object is excluded.
+;; modeline by (i) or (w) after the object type. With inner state, anything
+;; that would be considered delimiters or padding around an object is
+;; excluded.
 ;;
-;; The user can apply operations to objects. By marking objects before applying
-;; an operation, s?he can even operate on multiple objects at once. This works
-;; similar to the way you interact with files in `dired'. When marking an object
-;; the point moves on to the next object of this type.
+;; The user can apply operations to objects. By marking objects before
+;; applying an operation, s?he can even operate on multiple objects at once.
+;; This works similar to the way you interact with files in `dired'. When
+;; marking an object the point moves on to the next object of this type.
 ;;
 ;; The object type used for initialization is determined by the mapping of the
-;; entry command (see `objed-cmd-alist'). For example using `beginning-of-defun'
-;; will activate `objed' using the `defun' object as initial object type. With
-;; command `next-line', `objed' would initialize with the `line' object.
+;; entry command (see `objed-cmd-alist'). For example using
+;; `beginning-of-defun' will activate `objed' using the `defun' object as
+;; initial object type. With command `next-line', `objed' would initialize
+;; with the `line' object.
 ;;
-;; Objeds modal state provides basic movement commands which move by line, word
-;; or character. Those switch automatically to the corresponding object type,
-;; otherwise they work the same as the regular Emacs movement commands. Other
-;; commands only activate the part between the initial position and the new
-;; position moved to. By repeating commands you can often expand/proceed to
-;; other objects. This way you can compose movement and editing operations very
-;; efficiently.
+;; Objeds modal state provides basic movement commands which move by line,
+;; word or character. Those switch automatically to the corresponding object
+;; type, otherwise they work the same as the regular Emacs movement commands.
+;; Other commands only activate the part between the initial position and the
+;; new position moved to. By repeating commands you can often expand/proceed
+;; to other objects. This way you can compose movement and editing operations
+;; very efficiently.
 ;;
 ;; The expansion commands distinguish between block objects (objects built out
 ;; of lines of text) and context objects (programming constructs like strings,
 ;; brackets or textual components like sentences). This way you can quickly
 ;; expand to the desired objects.
 ;;
-;; For example to move to the end of the paragraph, the user would first move to
-;; the end of the line with "e". This would activate the text between the
+;; For example to move to the end of the paragraph, the user would first move
+;; to the end of the line with "e". This would activate the text between the
 ;; starting position and the end of the line. The user can now continue to the
 ;; end of the paragraph by by pressing "e" again. Now s?he is able to proceed
 ;; even further by pressing "e" again OR to continue by adding new text to the
@@ -90,8 +92,8 @@
 ;; animated demos.
 ;;
 ;; To learn more about available features and commands have a look at the
-;; descriptions below or the Docstrings and bindings defined in `objed-map'. To
-;; define your own operations and text objects see `objed-define-op' and
+;; descriptions below or the Docstrings and bindings defined in `objed-map'.
+;; To define your own operations and text objects see `objed-define-op' and
 ;; `objed-define-object'.
 ;;
 ;; Although some features are still experimental the basic user interface will
@@ -100,14 +102,13 @@
 ;;
 ;; CONTRIBUTE:
 ;;
-;; I'm happy to receive pull requests or ideas to improve this
-;; package. Some parts suffer from the bottom up approach of
-;; developing it, but this also allowed me to experiment a lot and try
-;; ideas while working on them, something that Emacs is especially
-;; good at. Most of the features are tested using `emacs-lisp-mode'
-;; but hopefully there aren't to many problems using modes for other
-;; languages, I tried my best to write text objects in a language
-;; agnostic way. Testing this and writing more tests in general would
+;; I'm happy to receive pull requests or ideas to improve this package. Some
+;; parts suffer from the bottom up approach of developing it, but this also
+;; allowed me to experiment a lot and try ideas while working on them,
+;; something that Emacs is especially good at. Most of the features are tested
+;; using `emacs-lisp-mode' but hopefully there aren't to many problems using
+;; modes for other languages, I tried my best to write text objects in a
+;; language agnostic way. Testing this and writing more tests in general would
 ;; be an important next step.
 ;;
 ;; This package would never been possible without the helpful community around
