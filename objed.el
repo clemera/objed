01f062187912785ebaa2961036802c777cbbc65d
Clemens Radermacher <clemera@posteo.net>
2019-12-29 14:19:10 +0100

    Version bump

--- objed.el
+++ objed.el
@@ -3,7 +3,7 @@
 
 ;; Author: Clemens Radermacher <clemera@posteo.net>
 ;; Package-Requires: ((emacs "25") (cl-lib "0.5"))
-;; Version: 0.8.2
+;; Version: 0.8.3
 ;; Keywords: convenience
 ;; URL: https://github.com/clemera/objed
 
